1cfed464d8bc7094ce71dadd325a8b055dd07057
Israel Santamaria Zarate <israelz961@gmail.com>
2024-05-27 20:14:44 -0700

    Ajuste de pocision de generacion de sol

--- Girasol.js
+++ Girasol.js
@@ -3,33 +3,32 @@ class Girasol {
         this.x = x;
         this.y = y;
         this.img = img;
-        this.lastProducedSol = millis(); // Guardar el momento en que se crea o produce el último sol
-        this.produceSolInterval = 2000; // Intervalo para producir soles (24 segundos)
+        this.lastProducedSol = millis(); 
+        this.produceSolInterval = 2000; 
     }
 
     draw() {
-        image(this.img, this.x, this.y, cellWidth, cellHeight); // Asumiendo que cellWidth y cellHeight son globales
+        image(this.img, this.x, this.y, cellWidth, cellHeight); 
     }
 
     update() {
         let currentTime = millis();
         if (currentTime - this.lastProducedSol > this.produceSolInterval) {
             this.generateSol();
-            this.lastProducedSol = currentTime; // Actualizar el último tiempo de generación
+            this.lastProducedSol = currentTime; 
         }
     }
 
     generateSol() {
-        // Código para generar un sol en la posición del girasol
-        let solX = this.x + cellWidth / 2;  // Asumiendo que queremos que aparezca cerca del girasol
-        let solY = this.y + cellHeight;     // Posición y un poco abajo del girasol
+        let solX = this.x + cellWidth / 2 - (cellWidth * 0.3 / 2); 
+        let solY = this.y;     
         soles.push({
-            img: spriteSheetSol.get(0, 0, 26, 26), // Asumiendo que tienes una sprite sheet para los soles
+            img: spriteSheetSol.get(0, 0, 26, 26), 
             x: solX,
             y: solY,
-            finalY: solY + 10,  // Hacer que caiga un poco
+            finalY: this.y + 10,  
             recolectado: false,
-            width: cellWidth * 0.3,  // Tamaño del sol
+            width: cellWidth * 0.3,  
             height: cellHeight * 0.3
         });
     }
